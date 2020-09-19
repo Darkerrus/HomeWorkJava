a05d8dc8b30ee5b58ed5f1e4ae11f107f7b74bfb
Darkerrus <56172007+Darkerrus@users.noreply.github.com>
2020-09-19 17:55:44 +0300

    Add files via upload

---
+++ 3-4/Author/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Author Adolf = new Author("Adolf", "AdolfH@war.Ger", 'm');
+        System.out.println(Adolf.toString());
+    }
+}
